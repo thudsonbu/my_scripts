aa737b3be10bf25e421649317f53e0005acf742d
thudsonbu <hudson_thom@bentley.edu>
2020-09-19 23:30:35 -0400

    Fixed change wallpaper script bug

--- change_wallpaper.py
+++ change_wallpaper.py
@@ -7,12 +7,11 @@ import os
 import requests
 import wget
 import subprocess, sys
+import time
 
 def get_wallpaper():
     access_key = os.environ.get('UNSPLASH_ACCESS_KEY')
-    tmp = os.environ.get('TMP')
-    print(access_key)
-    print(tmp)
+    tmp = os.environ.get('TEMP')
     url = 'https://api.unsplash.com/photos/random/?client_id='+access_key
     params = {
         "query": "HD Wallpapers",
@@ -20,14 +19,15 @@ def get_wallpaper():
     }
     response = requests.get(url,params).json()
     image_url = response['urls']['full']
-    wallpaper = wget.download(image_url, tmp) + ".jpg"
-    print(wallpaper)
+    wallpaper = wget.download(image_url, tmp)
+
     return wallpaper
 
 def main():
     wallpaper = get_wallpaper()
-    p = subprocess.Popen(["powershell.exe","Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value " + wallpaper + "\n" +
-    "rundll32.exe user32.dll, UpdatePerUserSystemParameters"],stdout=sys.stdout)
+    time.sleep(2)
+    p = subprocess.Popen(["powershell.exe","Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value " + wallpaper + '\n'
+    "rundll32.exe user32.dll, UpdatePerUserSystemParameters"], stdout=sys.stdout)
     p.communicate()
 
 if __name__ == '__main__':
